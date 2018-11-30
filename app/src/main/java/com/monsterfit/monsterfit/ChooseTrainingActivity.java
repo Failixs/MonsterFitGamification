1e3690364a1b1c6840afaa7e6958c3ea9151910b
FabiBa <fabian.bauer@student.uni-tuebingen.de>
2018-11-30 17:33:04 +0100

    added choose Training Activity

---
+++ app/src/main/java/com/monsterfit/monsterfit/ChooseTrainingActivity.java
@@ -0,0 +1,13 @@
+package com.monsterfit.monsterfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_training);
+    }
+}
