8fcde0f16ddf6dd51cfddd6d0100893bed57afe7
Fabiba <fabian.bauer@student.uni-tuebingen.de>
2019-01-05 00:09:05 +0100

    Changed start page to fit any screen (not tested!)

--- app/src/main/java/com/monsterfit/monsterfit/MainActivity.java
+++ app/src/main/java/com/monsterfit/monsterfit/MainActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -20,7 +28,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        // Get height of the display for layout issues
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int height = size.y;
+
+        ViewGroup.LayoutParams params;
+
+        // resize the views
+        TextView titleView = findViewById(R.id.titleView);
+        params = titleView.getLayoutParams();
+        params.height = (int)Math.ceil(height * 0.2);
+        titleView.setLayoutParams(new LinearLayout.LayoutParams(params));
+        titleView.invalidate();
+
+        ImageView usersPicture = findViewById(R.id.usersPicture);
+        params = usersPicture.getLayoutParams();
+        params.height = (int)Math.ceil(height * 0.6);
+        usersPicture.setLayoutParams(new LinearLayout.LayoutParams(params));
+        usersPicture.invalidate();
+
+        LinearLayout startPageButtons = findViewById(R.id.startPageButtons);
+        params = startPageButtons.getLayoutParams();
+        params.height = (int)Math.ceil(height * 0.2);
+        startPageButtons.setLayoutParams(new LinearLayout.LayoutParams(params));
+        startPageButtons.invalidate();
+
+
         //TextView timeInstalled = (TextView) findViewById(R.id.timeInstalledValue);
         //timeInstalled.setText(installedTime);
     }
